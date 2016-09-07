f0192319c9061264a88aed9c0f73fc32e7b6a4a0
daigou868 <hello@868daigou.com>
2016-09-07 18:29:31 +0930

    updates

--- lamp5.sh
+++ lamp5.sh
@@ -15,7 +15,7 @@ service ntp restart
 
 apt -y install apache2 php5 php5-common libapache2-mod-php5 php5-mcrypt php5-cgi php5-cli php5-curl php5-gd php5-mysql libcurl3 php5-curl php5-gd php5-mcrypt git php-soap
 
-echo "ServerName localhost" | sudo tee /etc/apache2/conf-available/fqdn.conf
+echo "ServerName 868daigou.cn.com" | sudo tee /etc/apache2/conf-available/fqdn.conf
 
 sed -i 's/index.html index.cgi index.pl index.php/index.php index.html index.cgi index.pl/g' /etc/apache2/mods-enabled/dir.conf
 
@@ -28,13 +28,13 @@ a2enconf fqdn
 a2enmod rewrite
 php5enmod mcrypt
 
-rm -rf /var/www/html/*
+#rm -rf /var/www/html/*
 
 wget https://github.com/OpenMage/magento-mirror/archive/1.9.2.4.tar.gz
 tar -xvzf 1.9.2.4.tar.gz
 sudo rsync -avP /root/magento-mirror-1.9.2.4/. /var/www/html/
 
-chmod 644 /var/www/html/.htaccess
+#chmod 644 /var/www/html/.htaccess
 
 for r in $(find / -name "php.ini"); do
 sed -i 's/memory_limit = 128M/memory_limit = 512M/g' $r;
@@ -47,11 +47,13 @@ echo "/etc/php5/cli/php.ini
 done
 
 chown -R www-data:www-data /var/www/html/
+find /var/www/html/ -type d -exec chmod 770 {} \;
+find /var/www/html/ -type f -exec chmod 660 {} \;
 
 service apache2 restart
 
 git clone https://github.com/letsencrypt/letsencrypt /opt/letsencrypt
-/opt/letsencrypt/letsencrypt-auto --apache -d 868daigou.cn.com -d www.868daigou.cn.com
+#/opt/letsencrypt/letsencrypt-auto --apache -d 868daigou.cn.com -d www.868daigou.cn.com
 
 useradd -m danijeljames -s /bin/bash
 echo "danijeljames:$DJpass" | chpasswd
